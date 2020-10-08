1f27a6290a61d5ed0dd42e1836d1569aed33d77e
Emma Rivera <72220543+emmarivera@users.noreply.github.com>
2020-10-08 13:59:10 -0500

    Agregue comentarios y cambios
    Se agrego el %f  y se agregaron comentarios

--- introduction/area.c
+++ introduction/area.c
@@ -1,11 +1,14 @@
-#include<stdio.h>
+#include<stdio.h> //Input Output
 #include<math.h>
-#define PI 3.1416
+
+#define PI 3.1416  //Definiendo una constante
 
 int main(){
-	float area, radio;
-	radio = 5;
-	area = PI * pow(5,2);
-	printf("Area\n");
-	printf("%s%f\n\n", "Area de Circulo con radio 5:", area);
+	float area, radio; //Declarando variables de tipo flotante
+	radio = 5; //Asignando valor a variable radio
+	area = PI * pow(5,2); //Asignando resultado de la operación a variable área
+	printf("Area\n"); // Imprimiendo un título
+	//Imprimiendo resultado
+	printf("%s%f\n\n", "Area de Circulo con radio 5: %f", area);
+	return 0;
 }
